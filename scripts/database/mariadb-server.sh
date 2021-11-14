fc7b5d2dacf191385ded387ec8f0be000cd04d13
Michael Nelson <revnelson@gmail.com>
2021-11-14 19:06:31 +0800

    Add mysql_secure_installation to mariadb-server script.

--- scripts/database/mariadb-server.sh
+++ scripts/database/mariadb-server.sh
@@ -38,6 +38,16 @@ if ! cmd_exists mysql; then
 
 fi
 
+# Run mysql_secure_installation
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' <<EOF | mysql_secure_installation
+      # current root password (emtpy after installation)
+    n # Set root password?
+    y # Remove anonymous users?
+    y # Disallow root login remotely?
+    y # Remove test database and access to it?
+    y # Reload privilege tables now?
+EOF
+
 # Seed server config file for SSL
 SSL_CONF_FILE="$DOTBASE/scripts/database/mariadb-server.cnf"
 SSL_CONF_DESTINATION="/etc/mysql/mariadb.conf.d/99-server-ssl.cnf"
