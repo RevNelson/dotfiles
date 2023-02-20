357865f535d04a1524320a3b225392f6e42aff36
Michael Nelson <revnelson@gmail.com>
2023-02-19 22:29:24 -0700

    Update php script to support versions and modules.
    - Easily edit an array to change php versions and modules
    
    Signed-off-by: Michael Nelson <revnelson@gmail.com>

--- scripts/webserver/php-update.sh
+++ scripts/webserver/php-update.sh
@@ -51,42 +51,57 @@ add-apt-repository --yes ppa:ondrej/php >/dev/null
 
 echo "Installing PHP and plugins..."
 
-apt_quiet update && apt_quiet install php8.2-fpm -y
+apt_quiet update
 
-# Install PHP extensions for WP
-apt_quiet install php8.2-bcmath php8.2-curl php8.2-gd php8.2-igbinary php8.2-imagick php8.2-intl \
-  php8.2-mbstring php8.2-mysql php8.2-redis php8.2-soap php8.2-xml php8.2-xmlrpc php8.2-zip
+versions=(
+    8.0
+    8.1
+    8.2
+)
 
-# echo "Adding optimized PHP settings..."
+modules=(
+    bcmath
+    curl
+    gd
+    igbinary
+    imagick
+    intl
+    mbstring
+    mysql
+    redis
+    soap
+    xml
+    xmlrpc
+    zip
+)
 
-# PHP_INI="$DOTBASE/scripts/webserver/php/php.ini"
-# PHP_FPM_CONF="$DOTBASE/scripts/webserver/php/php-fpm.conf"
+# install each version with corresponding modules
+for version in "${versions[@]}"; do
+    apt_quiet install "php$version-fpm"
+    for module in "${modules[@]}"; do
+        apt_quiet install "php$version-$module"
+    done
+done
 
-# PHP_BASE_CONFIG_DESTINATION="/etc/php/8.2/fpm/"
+echo "Adding PHP overrides..."
 
-# if [ -f $PHP_INI ]; then
-#   cp $PHP_INI $PHP_BASE_CONFIG_DESTINATION
-# fi
+for version in "${versions[@]}"; do
+    USER_PHP_FILE="$DOTBASE/scripts/webserver/php/overrides.ini"
+    USER_PHP_DESTINATION="/etc/php/$version/fpm/conf.d/99-overrides.ini"
 
-# if [ -f $PHP_FPM_CONF ]; then
-#   cp $PHP_FPM_CONF $PHP_BASE_CONFIG_DESTINATION
-# fi
+    if [ -f $USER_PHP_FILE ]; then
+        cp $USER_PHP_FILE $USER_PHP_DESTINATION
+    fi
 
-# echo "Adding PHP overrides..."
-
-USER_PHP_FILE="$DOTBASE/scripts/webserver/php/overrides.ini"
-USER_PHP_DESTINATION="/etc/php/8.2/fpm/conf.d/99-webserver-overrides.ini"
-
-if [ -f $USER_PHP_FILE ]; then
-  cp $USER_PHP_FILE $USER_PHP_DESTINATION
-fi
-
-if [ -f $USER_PHP_DESTINATION ]; then
-  echo "PHP overrides placed at $USER_PHP_DESTINATION"
-fi
+    if [ -f $USER_PHP_DESTINATION ]; then
+        echo "PHP $version overrides placed at $USER_PHP_DESTINATION"
+    fi
+done
 
 echo "Restarting PHP..."
 
-systemctl restart php8.2-fpm
+for version in "${versions[@]}"; do
+    systemctl restart "php$version-fpm"
+done
 
 echo "PHP is configured."
