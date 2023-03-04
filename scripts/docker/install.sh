0be792e64137e423516e3492cdb4d0eb723213d3
Michael Nelson <revnelson@gmail.com>
2023-03-03 22:37:36 -0700

    Add docker install script.

---
+++ scripts/docker/install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#
+##
+###
+#############
+# Variables #
+#############
+###
+##
+#
+
+# Check for USERNAME and set it if not found
+[[ -z ${USERNAME:-} ]] && USERNAME=${SUDO_USER:-$USER}
+
+#
+##
+###
+#############
+# Functions #
+#############
+###
+##
+#
+
+[[ -z ${DOTBASE:-} ]] && DOTBASE=$HOME/.dotfiles
+
+# Source function utils
+. $DOTBASE/functions/utils.sh
+
+# Make sure script is run as root.
+FILENAME=$(basename "$0" .sh)
+run_as_root $FILENAME
+
+#
+##
+###
+##########
+# Script #
+##########
+###
+##
+#
+
+apt update
+
+apt install apt-transport-https ca-certificates curl gnupg lsb-release software-properties-common
+
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
+
+apt update
+
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+usermod -aG docker ${USERNAME}
